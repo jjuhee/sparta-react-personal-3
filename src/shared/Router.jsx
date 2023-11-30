45d9169bc0b2e5afa084f2c1e74bfae157da035b
jjuhee <azaza.hee@gmail.com>
2023-11-30 15:44:37 +0900

    과제3번 라우터 세팅
    첫화면은 로그인화면만 보이게
    로그인 상태가 아니면 다른 페이지 접근불가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,27 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
+  const [isLogin, setIsLogin] = useState(false)
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isLogin ?
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+          </>
+          :
+          <>
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>
+        }
       </Routes>
     </BrowserRouter>
   );
